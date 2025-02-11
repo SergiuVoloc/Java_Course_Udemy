e885f16b291da21caa7b15a369b8b6fe4eaf459f
Sergiu Voloc <38067667+SergiuVoloc@users.noreply.github.com>
2025-02-11 19:06:57 +0000

    Initial

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,19 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Sergiu");
+
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("It's not an Alien");
+            System.out.println("And I'm afraid of Aliens");
+
+        }
+
+        int topScore = 100;
+        if (topScore == 100){
+            System.out.println("You have got the highest score!");
+        }
+
+    }
+}
