24747c7fdd1c6c30571fb98a2b8e6947ee330315
Sergiu Voloc <38067667+SergiuVoloc@users.noreply.github.com>
2025-02-12 11:03:40 +0000

    Initial

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -3,18 +3,60 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Sergiu");
 
-        boolean isAlien = false;
-        if (isAlien == false) {
-            System.out.println("It's not an Alien");
-            System.out.println("And I'm afraid of Aliens");
 
+
+//        boolean isAlien = false;
+//        if (isAlien == false) {
+//            System.out.println("It's not an Alien");
+//            System.out.println("And I'm afraid of Aliens");
+//            System.out.println("`------------------------`");
+//
+//        }
+//
+//        int topScore = 80;
+//        if (topScore < 100){
+//            System.out.println("You have got the highest score!");
+//        }
+//
+//        int secondTopScore = 95;
+//        if ((topScore > secondTopScore) && (topScore < 100)){
+//            System.out.println("Greater than second top score and less than 100!");
+//        }
+//
+//        if ((topScore > 90) || (secondTopScore <= 90)){
+//            System.out.println("One of the conditions are true!");
+//        }
+//
+//        boolean isCar = false;
+//        if (!isCar){
+//            System.out.println("It's a car");
+//        }
+//
+//
+//        String makeOfCar = "Ford";
+//        boolean isDomestic = (makeOfCar == "Ford") ? true : false;
+//
+//        if (isDomestic){
+//            System.out.println("I'm Domestic");
+//        }
+
+
+        double firstDouble = 20.00d;
+        double secondDouble = 80.00d;
+
+        double sum = (firstDouble + secondDouble) * 100.00d;
+        System.out.println("sum is " + sum);
+
+        double theRemainder = sum % 40.00d;
+        System.out.println("The remainder is " + theRemainder);
+
+        boolean isNoRemainder = theRemainder == 0 ? true : false;
+        System.out.println("The total is " + isNoRemainder);
+
+        if (!isNoRemainder) {
+            System.out.println("Got some remainder");
         }
 
-        int topScore = 100;
-        if (topScore == 100){
-            System.out.println("You have got the highest score!");
-            test
-        }
 
     }
 }
