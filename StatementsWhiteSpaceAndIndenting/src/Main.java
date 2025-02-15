bda8a568a79a6cf21ffec7b9c43ad33a09db9800
Sergiu Voloc <38067667+SergiuVoloc@users.noreply.github.com>
2025-02-15 22:30:54 +0000

    15/02

---
+++ StatementsWhiteSpaceAndIndenting/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int myVariable = 50;
+
+        myVariable++;
+        myVariable--;
+
+        System.out.println("This is " +
+                "a " +
+                "test");
+
+        if(myVariable == 0){
+            System.out.println("it's now zero");
+        }
+    }
+}
