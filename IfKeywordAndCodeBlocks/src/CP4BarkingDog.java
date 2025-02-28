bb180e7c279d91f270575d2c85050fde32637d94
Sergiu Voloc <38067667+SergiuVoloc@users.noreply.github.com>
2025-02-28 22:10:29 +0000

    25/02

---
+++ IfKeywordAndCodeBlocks/src/CP4BarkingDog.java
@@ -0,0 +1,28 @@
+public class CP4BarkingDog {
+    public static void main(String[] args) {
+
+        shouldWakeUp (true, 1);
+        shouldWakeUp (false, 2);
+        shouldWakeUp (true, 8);
+        shouldWakeUp (true, -1);
+        shouldWakeUp (true, 0);
+    }
+
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay){
+
+        if(barking == false || hourOfDay > 23 || hourOfDay < 0){
+            System.out.println("Sleep, Dog is not barking");
+            return false;
+        }else if(hourOfDay < 8 || hourOfDay == 23){
+            System.out.println("Wake Up! Dog is  barking");
+            return true;
+        }else {
+            System.out.println("Sleep, Dog is not barking");
+            return false;
+        }
+    }
+
+
+
+
+}
