e94fbb527545b0bd79812c5d4ab9ff18023e48f1
Sergiu Voloc <38067667+SergiuVoloc@users.noreply.github.com>
2025-03-26 22:40:26 +0000

    26/3/25

---
+++ IfKeywordAndCodeBlocks/src/CodingPractice/CP13_SwitchNumberInWord.java
@@ -0,0 +1,28 @@
+package CodingPractice;
+
+public class CP13_SwitchNumberInWord {
+    public static void main(String[] args) {
+
+        printNumberInWord(5);
+    }
+
+    public static void printNumberInWord(int number) {
+
+        String result = switch (number) {
+            case 0 -> "ZERO";
+            case 1 -> "ONE";
+            case 2 -> "TWO";
+            case 3 -> "THREE";
+            case 4 -> "FOUR";
+            case 5 -> "FIVE";
+            case 6 -> "SIX";
+            case 7 -> "SEVEN";
+            case 8 -> "EIGHT";
+            case 9 -> "NINE";
+            default -> "OTHER";
+        };
+        System.out.println(result);
+    }
+
+
+}
