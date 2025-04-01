67e45ec4ceda9351edb6603bc3f1917d772dd3a2
Sergiu Voloc <38067667+SergiuVoloc@users.noreply.github.com>
2025-04-01 22:31:42 +0100

    1/4/25

--- Loops/src/Main.java
+++ Loops/src/Main.java
@@ -1,2 +1,30 @@
 public class Main {
+    public static void main(String[] args) {
+
+        for(int counter = 1; counter <= 5; counter++){
+            System.out.println(counter);
+        }
+
+        for(double rate = 2; rate <= 5; rate++){
+            double interestAmount = calculateInterest(10000, rate);
+            System.out.println( rate + "%"+ " of 10k = " + interestAmount);
+        }
+
+
+        for(double i = 7.5; i <= 10; i+= 0.25){
+            double interestAmount = calculateInterest(100, i);
+            if(interestAmount > 8.5){
+                break;
+            }
+            System.out.println( i + "%"+ " of 10 = " + interestAmount);
+        }
+
+    }
+
+
+    public static double calculateInterest(double amount, double interestRate){
+        return (amount * (interestRate / 100));
+    }
+
+
 }
