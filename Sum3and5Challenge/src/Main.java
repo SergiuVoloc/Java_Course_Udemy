d2bfa77206d00ef9bc190ee73a62f27a6805480d
Sergiu Voloc <sergiuvlvoloc@gmail.com>
2025-08-20 22:59:30 +0100

    Challenge complete

---
+++ Sum3and5Challenge/src/Main.java
@@ -0,0 +1,24 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        int totalSum = 0;
+        int matchNumbersFound = 0;
+
+        for (int i = 1; i <= 1000; i++) {
+
+            if ((i % 5 == 0) && (i % 3 == 0 )){
+                matchNumbersFound++;
+                totalSum += i;
+                System.out.println("Match found: " + i);
+            }else if (matchNumbersFound == 5){
+            break;
+            }
+
+        }
+
+        System.out.println("The sum of numbers above is = " + totalSum);
+
+    }
+}
